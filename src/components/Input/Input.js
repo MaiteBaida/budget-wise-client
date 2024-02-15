e589b4dbce664802a4084073eb7f98c09fafdc4b
Maite Baida <maitemsabaida@gmail.com>
2024-02-15 13:19:11 -0700

    started login page, structure only

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,7 +2,16 @@ import './Input.scss';
 
 function Input() {
   return (
-    <input></input>
+    function Input({ name, placeholder, icon, customClass, onChange, value, type }) {
+      return (
+          <div className='input__container'>
+          <input name={name} className={`input ${customClass}`} placeholder={placeholder} onChange={onChange} value={value} type={type}></input>
+          {icon ? (
+            <img className="input__icon" src={icon}/>
+          ): null}
+        </div>
+      )
+  }
   );
 }
 
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,22 @@
 import './Login.scss';
+import Input from '../../components/Input/Input';
 
 function Login() {
     return (
       <main className="login">
+        <h1>Login</h1>
+        <div>
+          <label>Username:</label>
+          <Input />
+        </div>
+        <div>
+          <label>Password:</label>
+          <Input />
+        </div>
+        <div>
+          <label>Remember</label>
+          <input type='radio' id='remember' value='remember'></input>
+        </div>
       </main>
     );
   }
