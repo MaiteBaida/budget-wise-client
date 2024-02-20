97ff30224c813a697c14134b9581a49fde3c59b2
Maite Baida <maitemsabaida@gmail.com>
2024-02-20 16:24:03 -0700

    finished responsive styles

--- src/components/DeskTabTable/DeskTabTable.js
+++ src/components/DeskTabTable/DeskTabTable.js
@@ -1,13 +1,22 @@
 import "./DeskTabTable.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import edit from "../../assets/icons/edit.svg";
+import Button from "../Button/Button";
 
-function DeskTabTable({ list, title }) {
+function DeskTabTable({ list, title, expenseid, total }) {
+  const nav = useNavigate();
+
+  const handleSubmit = async (event) => {
+    nav(`/expenses/${expenseid}/newentry`);
+  };
   return (
     <section className="table">
-      <h2 className="table__title">{title} Expenses</h2>
+      <div className="table__title-container">
+        <h2 className="table__title">{title} Expenses</h2>
+        <span className="table__amount">${total} CAD</span>
+      </div>
       <div className="table__container">
-        <div className="table__row">
+        <div className="table__row table__row--header">
           <div className="table__header">EXPENSE</div>
           <div className="table__header">BUDGET</div>
           <div className="table__header">SPENT</div>
@@ -17,8 +26,16 @@ function DeskTabTable({ list, title }) {
             <div className="table__cell">{name}</div>
             <div className="table__cell">${budget} CAD</div>
             <div className="table__cell">${value} CAD</div>
-            <div className="table__cell">
-              <Link to={`/expense/${id}/edit`} className="table__link">
+            <div className="table__cell table__cell--buttons">
+              <div className="expenses__button-container">
+                <Button
+                  style="primary"
+                  type="button"
+                  label="Add entry"
+                  onSubmit={handleSubmit}
+                />
+              </div>
+              <Link to={`/expense/${id}/edit`}>
                 <img className="table__edit-icon" src={edit} />
               </Link>
             </div>
--- src/components/MobTable/MobTable.js
+++ src/components/MobTable/MobTable.js
@@ -1,28 +1,43 @@
 import "./MobTable.scss";
-import { Link } from "react-router-dom";
 import edit from "../../assets/icons/edit.svg";
-import { Value } from "sass";
+import Button from "../Button/Button";
+import { Link, useNavigate } from "react-router-dom";
 
-function MobTable({ list, title }) {
+function MobTable({ list, title, expenseid, total }) {
+  const nav = useNavigate();
+
+  const handleSubmit = async (event) => {
+    nav(`/expenses/${expenseid}/newentry`);
+  };
+
   return (
     <section className="expenses">
       <h2 className="expenses__title">{title} Expenses</h2>
+      <p className="expenses__amount">${total} CAD</p>
       {list.map(({ name, budget, value, id }) => (
         <div className="expenses__container">
-          <div className="expense__info">
+          <div className="expenses__info">
             <p className="expenses__header">EXPENSE</p>
             <p className="expenses__text">{name}</p>
           </div>
-          <div className="expense__info">
+          <div className="expenses__info">
             <p className="expenses__header">BUDGET</p>
-            <p className="expenses__text">{budget}</p>
-          </div>
-          <div className="expense__info">
-            <p className="expenses__header">SPENT</p>
-            <p className="expenses__text">{Value}</p>
+            <p className="expenses__text">$ {budget} CAD</p>
           </div>
           <div className="expenses__info">
-            <Link to={`/expense/${id}/edit`} className="expenses__link">
+            <p className="expenses__header">SPENT</p>
+            <p className="expenses__text">{value}</p>
+          </div>
+          <div className="expenses__buttons">
+            <div className="expenses__button-container">
+              <Button
+                style="primary"
+                type="button"
+                label="Add entry"
+                onSubmit={handleSubmit}
+              />
+            </div>
+            <Link to={`/expense/${id}/edit`}>
               <img className="expenses__edit-icon" src={edit} />
             </Link>
           </div>
