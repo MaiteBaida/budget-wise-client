03de8f9a6a9f93af96da3af1f22084f98d08b94b
Maite Baida <maitemsabaida@gmail.com>
2024-02-20 18:58:14 -0700

    expenses add styles, responsive

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Select.scss";
 
-function Select({ name, placeholder, customClass, onChange }) {
+function Select({ name, placeholder, customClass, onChange, options }) {
   return (
-    <select
-      name={name}
-      className={`select ${customClass}`}
-      placeholder={placeholder}
-      onChange={onChange}
-    />
+    <select name={name} className={`select ${customClass}`} onChange={onChange}>
+      <option value="">{placeholder}</option>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
   );
 }
 
--- src/pages/AddExpense/AddExpense.js
+++ src/pages/AddExpense/AddExpense.js
@@ -4,32 +4,59 @@ import Button from "../../components/Button/Button";
 import Select from "../../components/Select/Select";
 
 function AddExpense() {
+  const typeOptions = [
+    { value: "Fixed Expense", label: "Fixed Expense" },
+    { value: "Essential Expense", label: "Essential Expense" },
+    { value: "Non-Essential Expense", label: "Non-Essential Expense" },
+  ];
+
+  const frequencyOptions = [
+    { value: "Monthly", label: "Monthly" },
+    { value: "Weekly", label: "Weekly" },
+    { value: "Yearly", label: "Yearly" },
+  ];
+
   return (
-    <main className="item-add">
-      <h2>Add New Expense</h2>
-      <form>
-        <div>
-          <label>EXPENSE NAME</label>
-          <Input />
+    <main className="expenses-add">
+      <h2 className="expenses-add__title">Add New Expense</h2>
+      <form className="expenses-add__form">
+        <div className="expenses-add__info-container">
+          <div className="expenses-add__info">
+            <label className="expenses-add__label">EXPENSE NAME</label>
+            <Input customClass="expenses-add__input" />
+          </div>
+          <div className="expenses-add__info">
+            <label className="expenses-add__label">BUDGET</label>
+            <Input customClass="expenses-add__input" />
+          </div>
+          <div className="expenses-add__info">
+            <label className="expenses-add__label">EXPENSE TYPE</label>
+            <Select
+              options={typeOptions}
+              customClass="expenses-add__input"
+              required
+            />
+          </div>
+          <div className="expenses-add__info">
+            <label className="expenses-add__label">FREQUENCY</label>
+            <Select
+              options={frequencyOptions}
+              customClass="expenses-add__input"
+              required
+            />
+          </div>
         </div>
-        <div>
-          <label>BUDGET</label>
-          <Input />
+        <div className="expenses-add__button-container">
+          <div className="expenses-add__buttons">
+            <Button
+              customClass="expenses-add__button"
+              type="button"
+              style="secondary"
+              label="Cancel"
+            />
+            <Button type="submit" style="primary" label="Add" />
+          </div>
         </div>
-        <div>
-          <label>EXPENSE TYPE</label>
-          <Select required>
-            <option>Fixed Expense</option>
-            <option>Essential Expense</option>
-            <option>Non-Essential Expense</option>
-          </Select>
-        </div>
-        <div>
-          <label>FREQUENCY</label>
-          <Select required></Select>
-        </div>
-        <Button type="button" style="secondary" label="Cancel" />
-        <Button type="submit" style="primary" label="Add" />
       </form>
     </main>
   );
